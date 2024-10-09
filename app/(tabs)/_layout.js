72235609501faf5d259f3782c1065f6858220307
hydrazin60 <sureshkhdk45@gmail.com>
2024-10-09 20:58:33 +0545

    basic tab bar create

---
+++ app/(tabs)/_layout.js
@@ -0,0 +1,161 @@
+// import { Tabs } from "expo-router";
+// import Ionicons from "@expo/vector-icons/Ionicons";
+
+// export default function Layout() {
+//   return (
+//     <Tabs screenOptions={{ headerShown: false }}>
+//       <Tabs.Screen
+//         name="home"
+//         options={{
+//           title: "Home",
+//           tabBarIcon: ({ color, size }) => (
+//             <Ionicons name="home-outline" size={size} color={color} />
+//           ),
+//         }}
+//       />
+//       <Tabs.Screen
+//         name="friends"
+//         options={{
+//           title: "Friends",
+//           tabBarIcon: ({ color, size }) => (
+//             <Ionicons name="people-outline" size={size} color={color} />
+//           ),
+//         }}
+//       />
+// <Tabs.Screen
+//   name="button"
+//   options={{
+//     title: "",
+//     tabBarIcon: ({ color, size }) => (
+//       <View>
+//         <View
+//           style={{
+//             width: 29,
+//             height: 29,
+//             backgroundColor: "black",
+//             borderRadius: 4,
+//           }}
+//         >
+//           <Ionicons
+//             name="add-outline"
+//             size={30}
+//             color="white"
+//             style={{}}
+//           />
+//         </View>
+//       </View>
+//     ),
+//   }}
+// />
+//       <Tabs.Screen
+//         name="inbox"
+//         options={{
+//           title: "Inbox",
+//           tabBarIcon: ({ color, size }) => (
+//             <Ionicons name="chatbox-outline" size={size} color={color} />
+//           ),
+//         }}
+//       />
+//       <Tabs.Screen
+//         name="profile"
+//         options={{
+//           title: "Profile",
+//           tabBarIcon: ({ color, size }) => (
+//             <Ionicons name="person-outline" size={size} color={color} />
+//           ),
+//         }}
+//       />
+//     </Tabs>
+//   );
+// }
+
+import { View } from "react-native";
+import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+export default function Layout() {
+  return (
+    <Tabs
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarActiveTintColor: "black",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) =>
+            focused ? (
+              <Ionicons name="home" size={size} color={color} />
+            ) : (
+              <Ionicons name="home-outline" size={size} color={color} />
+            ),
+        }}
+      />
+      <Tabs.Screen
+        name="friends"
+        options={{
+          title: "Friends",
+          tabBarIcon: ({ focused, color, size }) =>
+            focused ? (
+              <Ionicons name="people" size={size} color={color} />
+            ) : (
+              <Ionicons name="people-outline" size={size} color={color} />
+            ),
+        }}
+      />
+      <Tabs.Screen
+        name="button"
+        options={{
+          title: "",
+          tabBarIcon: ({ color, size }) => (
+            <View>
+              <View
+                style={{
+                  width: 29,
+                  height: 29,
+                  backgroundColor: "black",
+                  borderRadius: 4,
+                }}
+              >
+                <Ionicons
+                  name="add-outline"
+                  size={30}
+                  color="white"
+                  style={{}}
+                />
+              </View>
+            </View>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="inbox"
+        options={{
+          title: "Inbox",
+          tabBarIcon: ({ focused, color, size }) =>
+            focused ? (
+              <Ionicons name="chatbox" size={size} color={color} />
+            ) : (
+              <Ionicons name="chatbox-outline" size={size} color={color} />
+            ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ focused, color, size }) =>
+            focused ? (
+              <Ionicons name="person" size={size} color={color} />
+            ) : (
+              <Ionicons name="person-outline" size={size} color={color} />
+            ),
+        }}
+      />
+    </Tabs>
+  );
+}
